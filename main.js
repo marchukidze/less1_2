22d00b044e28dc7db7c2520234f19420111ebd3a
irma <marchukidze@gmail.com>
2020-11-14 17:46:50 +0200

    add js

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let a = prompt('введите переменную a', "");
+let b = prompt('введите переменную b', "");
+let c = prompt('введите переменную с', "");
+
+console.log(+a + +b + +c);
+
+if (a % 2 == 0) {
+    console.log('четное число');
+} else {
+    console.log('нечетное число');
+}
+
+if (b % 2 == 0) {
+    console.log('четное число');
+} else {
+    console.log('нечетное число');
+}
+
+if (c % 2 == 0) {
+    console.log('четное число');
+} else {
+    console.log('нечетное число');
+}
